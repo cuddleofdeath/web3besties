10c7b14a8b7e76af831959d196803b2d0d6163c0
CuddleofDeath <cuddleofdeath@gmail.com>
2023-01-04 16:13:56 -0600

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        pathname: '/profile_images/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        pathname: '/media/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        pathname: '/profile_banners/*',
+      },
+    ],
+  },
+};
+module.exports = nextConfig;
